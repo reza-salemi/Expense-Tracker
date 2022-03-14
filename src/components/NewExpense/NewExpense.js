fa360522534065b71f897c196a9b119ed126bb4a
Mohammad Reza Salemi <en.salemi75@gmail.com>
2022-03-14 21:07:12 +0330

    pass data from child to parent component

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,17 @@
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const expenseDataHandler = (enteredExpenseDate) => {
+    const expenseData = {
+      ...enteredExpenseDate,
+      id: Math.random().toString(),
+    };
+    props.onAddExpense(expenseData);
+  };
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={expenseDataHandler} />
     </div>
   );
 };
